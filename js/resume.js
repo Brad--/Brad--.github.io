86136ee5c127fb3c60bd01716a9197e9a4e5a4ee
Marrow- <brad.smith.1324@gmail.com>
2015-12-19 20:27:40 -0700

    Updated Resume
    Updated personal resume

---
+++ js/resume.js
@@ -0,0 +1,19 @@
+// #035096 My blue!
+// rgb(162, 156, 170)
+$(document).ready(function() {
+  $("#jquery").click(function(){
+    $("#jquery").effect("shake", "slow");
+  });
+
+  var $root = $('html,body');
+  $('a').on('click', function(e) {
+    if($(this).href.substr(0, 5) === 'https')
+      return;
+      
+    e.preventDefault();
+    $root.animate({
+      scrollTop:$('[name="' + $.attr(this, 'href').substr(1) + '"]')
+        .offset().top - 50}, 500);
+  });
+
+});
